a0de536ea8387044f357271b33f9d1585668bf5f
lleyvah <lleyvah.769@gmail.com>
2022-06-12 20:42:38 -0500

    feat(app v1)

---
+++ src/main/java/com/microservice/budget/BsBudgetV1Application.java
@@ -0,0 +1,13 @@
+package com.microservice.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsBudgetV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsBudgetV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/budget/TestAccount.java
@@ -0,0 +1,4 @@
+package com.microservice.budget;
+
+public class TestAccount {
+}
